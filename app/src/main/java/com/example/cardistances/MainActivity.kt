10a1efd9fd569ce947be772e73ad1b9a03d3e167
Vinny-23 <alvingore70@gmail.com>
2026-04-30 22:53:53 +0200

    Added the first files

--- app/src/main/java/com/example/cardistances/MainActivity.kt
+++ app/src/main/java/com/example/cardistances/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.cardistances
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //ViewCompat
+        //Code goes below here
+        //Declarations
+        //UI Elements
+
     }
+    val edtAmount = findViewById<EditText>(R.id.etdAmount)
+    val edtDistance = findViewById<EditText>(R.id.edtDistance)
+    val btnRange = findViewById<Button>(R.id.btnRange)
+    val edtResult = findViewById<TextView>(R.id.edtResult)
+
+
 }
